632730bce72de9e811d98c707ecf4062e3418501
kranthi <98206178+kranthi304@users.noreply.github.com>
2023-03-05 12:38:32 +0530

    Add files via upload

---
+++ Meta.py
@@ -0,0 +1,171 @@
+import streamlit as st
+
+st.header('2020-2024')
+
+st.title('Btech --- Metallurgical and materials Engineering (Nit Rourkela)')
+
+
+import streamlit as st
+
+st.header(":red[Meta Web Application] ")
+st.subheader(":red[Let's Start Baby]")
+st.snow()
+
+
+btn_click = st.button("Click Here to Know Birthdays")
+if btn_click == True:
+    st.subheader("Kranthi Reddy--- August 12 ")
+    st.subheader("Yaswanth--- Septermber 14 ")
+    st.subheader("Jayasree--- August 9 ")
+    st.subheader("Ganesh--- August 5 ")
+    st.subheader("Mukesh--- August 12 ")
+    st.subheader("Kalyansrinivas--- January 4 ")
+    st.subheader("Sai Bharadwaja--- August 12 ")
+    st.subheader("Sai Chandu--- August 12 ")
+    st.subheader("Vamshidar Reddy--- August 12 ")
+    st.subheader("Shreehitha--- August 12 ")
+    st.subheader("Srilekha--- August 12 ")
+    st.subheader("Vinay--- August 12 ")
+    st.subheader("Abhinay Karthik--- August 12 ")
+    st.subheader("Surya--- August 12 ")
+    st.subheader("Dileep --- August 12 ")
+    st.subheader("Nagendra--- August 12 ")
+    st.subheader("Nikhil--- August 12 ")
+    
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Guntur')
+
+btn_click = st.button("Click Here to Know Kranthi")
+
+if btn_click == True:
+    st.subheader("Kranthi Reddy")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Guntur')
+    
+    # st.subheader(":red[Let's Start Baby]")
+    
+    
+# from matplotlib import image
+# import os
+
+# # st.title("Dashboard - Titanic Data")
+
+# FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# # absolute path to this file's root directory
+# PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# # absolute path of directory_of_interest
+# dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+# IMAGE_PATH = os.path.join(dir_of_interest,"images", "kranthi_puri.jpg")
+# img = image.imread(IMAGE_PATH)
+# st.image(img)
+
+
+
+
+btn_click = st.button("Click Here to Know Sai Chandu")
+
+if btn_click == True:
+    st.subheader("Sai Chandu")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Vijayawada')
+    # st.subheader(":red[Let's Start Baby]")
+    
+btn_click = st.button("Click Here to Know Sai Bharadwaja")
+
+if btn_click == True:
+    st.subheader("Sai Bharadwaja")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Ongole')
+    # st.subheader(":red[Let's Start Baby]")
+    
+    
+    
+btn_click = st.button("Click Here to Know Yaswanth")
+
+if btn_click == True:
+    st.subheader("Yaswanth")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Srikaklulam')
+    # st.subheader(":red[Let's Start Baby]")
+    
+    
+btn_click = st.button("Click Here to Know Surya")
+
+if btn_click == True:
+    st.subheader("Surya")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Telangana')
+    # st.subheader(":red[Let's Start Baby]")
+
+btn_click = st.button("Click Here to Know Ganesh")
+
+if btn_click == True:
+    st.subheader("Ganesh")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Srikakulam')
+    # st.subheader(":red[Let's Start Baby]")
+
+btn_click = st.button("Click Here to Know Vinay")
+
+if btn_click == True:
+    st.subheader("Vinay")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Rajahmundry')
+    # st.subheader(":red[Let's Start Baby]")
+
+btn_click = st.button("Click Here to Know Kalyansrinivas")
+
+if btn_click == True:
+    st.subheader("Kalyansrinivas")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Vizag')
+    # st.subheader(":red[Let's Start Baby]")
+    
+btn_click = st.button("Click Here to Know Abhinay Karthik")
+
+if btn_click == True:
+    st.subheader("Abhinay Karthik")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Vizag')
+    # st.subheader(":red[Let's Start Baby]")
+
+
+btn_click = st.button("Click Here to Know Sri Lekha")
+
+if btn_click == True:
+    st.subheader("Sri Lekha")
+    
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('West Godavari')
+    
+    # st.subheader(":red[Let's Start Baby]")
+    
+btn_click = st.button("Click Here to Know Jayasree")
+
+if btn_click == True:
+    st.subheader("Jayasree")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Vizag')
+    # st.subheader(":red[Let's Start Baby]")
+btn_click = st.button("Click Here to Know Shreehitha")
+
+if btn_click == True:
+    st.subheader("Shreehitha")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Hyderabad')
+    # st.subheader(":red[Let's Start Baby]")
+btn_click = st.button("Click Here to Know Mukesh")
+
+if btn_click == True:
+    st.subheader("Mukesh")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Vizianagaram')
+    # st.subheader(":red[Let's Start Baby]")
+btn_click = st.button("Click Here to Know Nikhil")
+
+if btn_click == True:
+    st.subheader("Nikhil")
+    # st.subheader("Currently persuing Btech at  National Institute of Technology Rourkela")
+    st.header('Warangal')
+    # st.subheader(":red[Let's Start Baby]")
